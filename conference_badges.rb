5e33ee0786fa0617b2d0830dfe2f6fdacbb63dc2
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2019-09-18 23:33:16 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,13 +20,5 @@ def assign_rooms(attendees)
   end
 end
 
-def printer(attendees)
-  
-  batch_badge_creator(attendees).each do |badge|
-    puts badge
-  end
-  
-  assign_rooms(attendees).each do |assignment|
-    puts assignment
-end
+
 
